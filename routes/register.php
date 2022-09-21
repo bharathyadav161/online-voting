2c85bf46153755ffdb06d475c2889c9e25a1b9f9
bharathyadav161 <97655326+bharathyadav161@users.noreply.github.com>
2022-09-21 16:34:40 +0530

    Add files via upload

---
+++ routes/register.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>Online voting system - Registratrion</title>
+        <link rel="stylesheet" href="../css/stylesheet.css">
+		<style>
+		body{
+			background-image:url("IMR.jpg");
+			background-repeat:no-repeat;
+			background-size:100% 100%;
+		}
+		</style>
+    </head>
+    <body>
+        <center>
+            <div id="headerSection">
+            <h1>	E  VOTING</h1>  
+            </div>
+            <hr>
+
+            <h2>Registration</h2>
+                <form action="../api/register.php" method="POST" enctype="multipart/form-data">
+                    <input type="text" name="name" placeholder="Name" required>&nbsp
+                    <input type="number" name="mob" placeholder="Mobile" required><br><br>
+                    <input type="password" name="pass" placeholder="Password" required>&nbsp
+                    <input type="password" name="cpass" placeholder="Confirm Password" required><br><br>
+                    <input style="width: 31%" type="text" name="add" placeholder="Address" required><br><br>
+                    <div id="upload" style="width: 30%">
+                        Upload image: <input type="file" id="profile" name="image" required>
+                    </div><br>
+                    <div id="upload" style="width: 30%">
+                        Select your role:
+                        <select name="role">
+						
+                            <option value="1">Voter</option>
+                            <option value="2">Candidate</option>
+                        </select><br>                   
+                    </div><br>
+                    <button id="loginbtn" type="submit" name="registerbtn">Register</button><br><br>
+                    Already user? <a href="../">Login here</a>
+                </form>
+            </center>
+    </body>
+</html>
